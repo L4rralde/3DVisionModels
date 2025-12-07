fa902bfbea261e0da41519135784c2f5be3810f3
l4rralde <ealarralde@gmail.com>
2025-12-07 16:29:24 -0600

    Adding code task to align clusters from hierarchical clustering

--- src/glueing/__init__.py
+++ src/glueing/__init__.py
@@ -2,3 +2,4 @@ from .types import FeaturesDf
 from .filter import filter_features_df
 from .clustering import hierarchical_clustering, ConnectedClusterTree
 from .align import est_scenes_transform, transform_scene
+from .graph import Tree as SceneTree
